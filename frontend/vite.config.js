13c1934d0d71b3768b586a01a7cfcf2c59f7173a
TEJANSH TIWARI <tejanshtiwari2410@gmail.com>
2026-09-15 15:37:42 +0000

    viteconfig and api remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://trackon-web-backend.onrender.com',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://trackon-web-backend.onrender.com',
         ws: true,
       }
     }
